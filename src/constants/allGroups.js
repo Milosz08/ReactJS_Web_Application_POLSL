a146c69d27c4fe170bd001af7dec3cf7a604274b
Milosz08 <gilgamilosz451@gmail.com>
2021-08-04 03:33:59 +0200

    add groups static datas (pure JS)

---
+++ src/constants/allGroups.js
@@ -0,0 +1,20 @@
+const GROUPS_STATIC = {
+   NORMAL_GROUPS: [
+      {
+         field: 'firstGroup',
+         text: 'Pierwsza',
+      },
+      {
+         field: 'secondGroup',
+         text: 'Druga',
+      }
+   ],
+   ENG_GROUPS: [
+      'b2a',
+      'b2b',
+      'c1a',
+      'c1b',
+   ],
+};
+
+export default GROUPS_STATIC;
