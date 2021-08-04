8447d6d85f71425725c4e25f0e84c6ff9ae90530
Milosz08 <gilgamilosz451@gmail.com>
2021-08-04 03:32:05 +0200

    add static objects (pure JS)

---
+++ src/constants/inputOptions.js
@@ -0,0 +1,15 @@
+const STATIC_OPTIONS = {
+   SEMESTERS: [
+      'pierwszy', 'drugi', 'trzeci', 'czwarty', 'piaty', 'szósty', 'siódmy',
+   ],
+   TYPE_OPTIONS: [
+      'Wykłady', 'Ćwiczenia', 'Laboratoria', 'Konwersatoria', 'Seminaria', 'Warsztaty', 'Repetytoria',
+      'Wszystkie Zajęcia'
+   ],
+   PLATFORMS_OPTIONS: [
+      'Microsoft Teams', 'Google Classroom', 'Hangouts Meet', 'Zoom', 'Skype', 'Slack', 'Facebook', 'WhatsApp',
+      'Big Blue Button', 'Cisco Webex Teams',
+   ],
+};
+
+export default STATIC_OPTIONS;
---
+++ src/constants/navigationStatic.js
@@ -0,0 +1,28 @@
+const STATIC_NAV = [
+   {
+      icon: 'home',
+      alt: 'Przejź do głównej zakładki',
+   },
+   {
+      icon: 'virus',
+      alt: 'Przejź do ustawień informacji o Covid-19',
+   },
+   {
+      icon: 'chalkboard',
+      alt: 'Przejź do ustawień zarządzania przedmiotami',
+   },
+   {
+      icon: 'clock',
+      alt: 'Przejź do zarządzania planem zajęć',
+   },
+   {
+      icon: 'calendar-alt',
+      alt: 'Przejdź do zarządzania wpisami do kalendarza',
+   },
+   {
+      icon: 'envelope',
+      alt: 'Przejdź do zarządzania wiadomościami użytkowników',
+   },
+];
+
+export default STATIC_NAV;
