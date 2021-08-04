ae47961f8963663162c7914042ffa58ce3646129
Milosz08 <gilgamilosz451@gmail.com>
2021-08-05 00:34:06 +0200

    add GotoTopButton component (with styles)

---
+++ src/components/layouts/GotoTopButton/GotoTopButton.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import classnames from "classnames";
+
+const { gotoTopWrapper, showGotoButton } = require('./GotoTopButton.module.scss');
+
+/**
+ * Komponent dodający przycik (w pozycji fixed) umożliwiający po kliknięciu przejście do góry strony.
+ * Przycisk widoczny jest, tylko gdy pozycja scrolla (od szczytu strony) przekroczy wartość 200 jednostek.
+ */
+const GotoTopButton = () => {
+
+   const [ offset, setOffset ] = useState<number>(0);
+   const [ showGoto, setShowGoto ] = useState<boolean>(false);
+
+   useEffect(() => {
+      const handleOnScroll = () => {
+         if(offset > 200) {
+            setShowGoto(false);
+         } else {
+            setShowGoto(true);
+         }
+         setOffset(window.pageYOffset);
+      }
+      window.addEventListener('scroll', handleOnScroll);
+      return () => window.removeEventListener('scroll', handleOnScroll);
+   }, [offset]);
+
+   const handleClickGotoTop = () => document.body.scrollIntoView({ behavior: 'smooth' });
+   const toggleClass = showGoto ? showGotoButton : '';
+
+   return (
+      <button
+         className = {classnames(gotoTopWrapper, toggleClass)}
+         onClick = {handleClickGotoTop}
+      />
+   );
+}
+
+export default GotoTopButton;
