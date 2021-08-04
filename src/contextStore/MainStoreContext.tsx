71f3c3d8808dfb0e4cefc08487e8c813a9b0e866
Milosz08 <gilgamilosz451@gmail.com>
2021-08-04 03:29:02 +0200

    add mainStoreContext (types in TS and implements in component)

---
+++ src/contextStore/MainStoreContext.tsx
@@ -0,0 +1,49 @@
+import React, { createContext, useEffect, useState } from 'react';
+import axiosInstance from "../helpers/request";
+import { MainStoreStateProvider } from "./MainStoreStateProvider";
+
+export const MainStoreContext = createContext<any>(null);
+
+interface PropsProvider {
+   children: React.ReactNode;
+}
+
+/**
+ * Główny store pobierający i przechowujący niewrażliwe dane z serwera (komunikacja przy wykorzystaniu API z
+ * backendu). Dziedziczą go wszystkie węzły aplikacji (store globalny).
+ *
+ * @param children - wszystkie węzły dziedziczące zawartość stora.
+ */
+export default function MainStoreProvider({ children } : PropsProvider) {
+
+   const [ dataFetchFromServer, setDataFetchFromServer ] = useState<MainStoreStateProvider>({
+      covidData: [], footerForms: [], subjectsData: [], sheduleSubjects: [], calendarRecords: []
+   });
+
+   useEffect(() => {
+      const getAllData = async () => {
+         const covidData = await axiosInstance.get('/covid-data');
+         const footerForm = await axiosInstance.get('/footer-form');
+         const subjectsData = await axiosInstance.get('/subjects-data');
+         const subjectShedule = await axiosInstance.get('/subject-shedule');
+         const calendarRecord = await axiosInstance.get('/calendar-record');
+
+         setDataFetchFromServer({
+            covidData: JSON.parse(covidData.request.response),
+            footerForms: JSON.parse(footerForm.request.response),
+            subjectsData: JSON.parse(subjectsData.request.response),
+            sheduleSubjects: JSON.parse(subjectShedule.request.response),
+            calendarRecords: JSON.parse(calendarRecord.request.response)
+         });
+      }
+      getAllData();
+   }, []);
+   
+   return (
+      <MainStoreContext.Provider
+         value = {{ dataFetchFromServer, setDataFetchFromServer }}
+      >
+         {children}
+      </MainStoreContext.Provider>
+   );
+}
---
+++ src/contextStore/MainStoreStateProvider.ts
@@ -0,0 +1,58 @@
+/**
+ * Interfejs typów obiektów przechwytywanych z bazy danych. Interfejs eksportowany do głównego stora
+ * (MainStoreContext) przechowującego w obiekcie wszystkie pobrane dane z API
+ */
+export interface MainStoreStateProvider {
+   covidData: Array<{
+      _id: string,
+      description: string,
+      actualRiskNumber: number,
+      __v: number
+   }>;
+   footerForms: Array<{
+      _id: string,
+      userIdentity: string,
+      userChoice: string,
+      userMessage: string,
+      sendDate: {
+         fullDate: string,
+         fullTime: string
+      }, __v: number
+   }>;
+   subjectsData: Array<{
+      _id: string,
+      semesters: Array<string>,
+      departments: Array<string>,
+      icon: Array<string>,
+      classesPlatform: Array<{
+         type: string,
+         place: string,
+         link: string
+      }>,
+      title: string,
+      __v: number
+   }>;
+   sheduleSubjects: Array<{
+      _id: string,
+      title: string,
+      group: string,
+      day: string,
+      type: string,
+      start: string,
+      end: string,
+      pzeInfo: {
+         platform: string,
+         pzeLink: string
+      }
+   }>;
+   calendarRecords: Array<{
+      day: number,
+      month: number,
+      year: number,
+      items: Array<{
+         start: string,
+         message: string,
+         importantLevel: string
+      }>
+   }>;
+}
