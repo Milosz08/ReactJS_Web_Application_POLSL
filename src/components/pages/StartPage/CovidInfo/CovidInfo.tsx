d74a0181629daf0f5b3eb31782d86b6128f906ae
Milosz08 <gilgamilosz451@gmail.com>
2021-08-04 03:55:12 +0200

    update covidInfo component (with styles)

--- src/components/pages/StartPage/CovidInfo/CovidInfo.tsx
+++ src/components/pages/StartPage/CovidInfo/CovidInfo.tsx
@@ -1,7 +1,38 @@
 import React, { useContext } from 'react';
+import { MainStoreContext } from "../../../../contextStore/MainStoreContext";
 
+const { covidBlocks, covidInfo } = require('./CovidInfo.module.scss');
+
+/**
+ * Komponent strony głównej informujący o aktualnym poziomie zagrożenia epidemiologicznego na terenie
+ * Politechniki Śląskiej. Dane są pobierane z głównego stora.
+ */
 export default function CovidInfo() {
+
+   const { dataFetchFromServer } = useContext<any>(MainStoreContext);
+   const { covidData } = dataFetchFromServer;
+
+   const generateCovidInfos = covidData.map((info: any) => {
+      return (
+         <div
+            className = {covidBlocks}
+            key = {info._id}
+         >
+            <span>{info.description}:</span>
+            <span>{info.actualRiskNumber}</span>
+         </div>
+      );
+   });
+
    return (
-      <div>CovidInfo</div>
+      <div className = {covidInfo}>
+         <div>Status zabezpieczeń<br/>COVID-19</div>
+         <a
+            href = 'https://covid.polsl.pl/'
+            target = '_blank'
+            rel = 'noreferrer'
+         >więcej informacji</a>
+         {generateCovidInfos}
+      </div>
    );
 }
