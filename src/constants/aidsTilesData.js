53ecdb7ffd1e4ea0afcaefa774fec8f63fd7a97c
Milosz08 <gilgamilosz451@gmail.com>
2021-08-06 15:57:19 +0200

    add static tiles helpPage data (pure JS)

---
+++ src/constants/aidsTilesData.js
@@ -0,0 +1,29 @@
+const TILES_DATA = [
+   {
+      title: 'Grupa MS Teams',
+      link: 'https://teams.microsoft.com/l/team/19%3aUMGgtKGt9r1uF-vocetHPzvoU4CqDlY0gEAknZ3BIJ81%40thread.tacv2/conversations?groupId=beeca57e-2aa4-43f4-bc6c-89bfd727d12c&tenantId=ab840be7-206b-432c-bd22-4c20fdc1b261',
+      icon: ['fas', 'user-friends'],
+   },
+   {
+      title: 'Grupa Facebook',
+      link: 'https://www.facebook.com/groups/3622273614473972',
+      icon: ['fab', 'facebook-square'],
+   },
+   {
+      title: 'Grupa Discord',
+      link: 'https://discord.gg/wkJG8uaYPx',
+      icon: ['fab', 'discord'],
+   },
+   {
+      title: 'Dysk I semestr',
+      link: 'https://mega.nz/folder/hjwCFC4A#K3bnSBoMNK30bcQCNbP0Dw',
+      icon: ['fas', 'database'],
+   },
+   {
+      title: 'Dysk własne materiały',
+      link: 'https://mega.nz/folder/QiwEXSJK#btpZcXAhqPX4MjKh3mAngQ',
+      icon: ['fas', 'database'],
+   },
+];
+
+export default TILES_DATA;
