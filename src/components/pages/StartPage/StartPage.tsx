ccafc4c1cf3efc9701fbcbcc511319cbf36a78fb
Milosz08 <gilgamilosz451@gmail.com>
2021-08-06 16:06:04 +0200

    create StartPage components structure

--- src/components/pages/StartPage/StartPage.tsx
+++ src/components/pages/StartPage/StartPage.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-export default function StartPage() {
+import CookiesNotification from '../../layouts/CookiesNotification/CookiesNotification';
+import Header from '../../layouts/Header/Header';
+import Slider from './Slider/Slider';
+import CovidInfo from './CovidInfo/CovidInfo';
+import CountDown from './CountDown/CountDown';
+import Navigation from '../../layouts/Navigation/Navigation';
+import Subjects from '../../layouts/Subjects/Subjects';
+
+/**
+ * Strona startowa (adres bezwzględny)
+ */
+const StartPage = () => {
    return (
-      <div>StartPage</div>
+      <Fragment>
+         <CookiesNotification/>
+         <Header ifHeaderHasRedBar = {false}/>
+         <Slider autoPlay = {true} duration={5}/>
+         <CovidInfo/>
+         <CountDown/>
+         <Navigation ifHeader={false}/>
+         <Subjects/>
+      </Fragment>
    );
 }
+
+export default StartPage;
