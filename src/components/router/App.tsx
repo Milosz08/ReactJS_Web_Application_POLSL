a00ce3c3c557facba1e377b61ee3ce1d309b11a1
Milosz08 <gilgamilosz451@gmail.com>
2021-08-08 03:25:23 +0200

    actualised App component DOM structure

--- src/components/router/App.tsx
+++ src/components/router/App.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import { CookiesProvider } from 'react-cookie';
 
-import CookiesNotification from "../layouts/CookiesNotification/CookiesNotification";
+import CookiesNotification from '../layouts/CookiesNotification/CookiesNotification';
+import MainStoreProvider from '../../contextStore/MainStoreContext';
+import StartPage from '../pages/StartPage/StartPage';
+import CookiesObjectsProvider from '../../contextStore/CookiesObjectsProvider';
+import Slider from '../pages/StartPage/Slider/Slider';
+import Subjects from '../layouts/Subjects/Subjects';
+import CountDown from '../pages/StartPage/CountDown/CountDown';
+import CovidInfo from '../pages/StartPage/CovidInfo/CovidInfo';
+import ActualDateProvider from '../../contextStore/ActualDateProvider';
+import GotoTopButton from '../layouts/GotoTopButton/GotoTopButton';
+import LoginSessionProvider from '../../contextStore/LoginSessionProvider';
+import Page from './Page';
+import DevToolsInfo from '../layouts/DevToolsInfo/DevToolsInfo';
+import Footer from '../layouts/Footer/Footer';
 
-import { CookiesProvider } from "react-cookie";
-import MainStoreProvider from "../../contextStore/MainStoreContext";
-import StartPage from "../pages/StartPage/StartPage";
+import './../../constants/fontAwesomeInject';
 
 /**
- *
+ * Główny komponent, generuje całą strukturę aplikacji.
  */
-export default function App() {
+const App = () => {
    return (
       <MainStoreProvider>
-         {/*<CookiesProvider>*/}
-            {/*<CookiesNotification/>*/}
-            <StartPage/>
-         {/*</CookiesProvider>*/}
+         <CookiesProvider>
+            <CookiesObjectsProvider>
+               <Router>
+                  <GotoTopButton/>
+                  <LoginSessionProvider>
+                     <Page/>
+                  </LoginSessionProvider>
+                  <DevToolsInfo/>
+                  <Footer/>
+               </Router>
+            </CookiesObjectsProvider>
+         </CookiesProvider>
       </MainStoreProvider>
    );
 }
+
+export default App;
