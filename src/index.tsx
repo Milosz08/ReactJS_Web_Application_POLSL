385937c3a0375bacad3abd4f1a512d1c5c6330a7
Milosz08 <gilgamilosz451@gmail.com>
2021-08-08 03:15:08 +0200

    add turn off animations on reside global listener

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,21 @@ import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './components/router/App';
 
+/**
+ * Wymuszenie przy każdym przeładowaniu strony scrolla na pozycji X:0, Y:0
+ */
+window.onbeforeunload = () => window.scrollTo(0, 0);
+
+/**
+ * Wyłącznie animacji na czas zmiany rozmiaru okna przeglądarki
+ */
+let resizeTimer: NodeJS.Timeout;
+window.addEventListener('resize', ():void => {
+   document.body.classList.add('stopTransitions');
+   clearTimeout(resizeTimer);
+   resizeTimer = setTimeout(():void => {
+      document.body.classList.remove('stopTransitions');
+   }, 400)
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
