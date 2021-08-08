b9f04871fe2596cb6870fba8d0a4725d3416759f
Milosz08 <gilgamilosz451@gmail.com>
2021-08-08 03:25:54 +0200

    initialise react function component

---
+++ src/components/pages/AdminCmsSystemPage/AdminCmsMainPanel/AdminCmsLeftNavigation.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminCmsLeftNavigation = () => {
+   return (
+      <div>1234</div>
+   )
+}
+
+export default AdminCmsLeftNavigation;
---
+++ src/components/pages/AdminCmsSystemPage/AdminCmsMainPanel/AdminCmsMainPanel.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AdminCmsMainPanel = () => {
+   return (
+      <div>AdminCmsMainPanel</div>
+   );
+}
+
+export default AdminCmsMainPanel;
