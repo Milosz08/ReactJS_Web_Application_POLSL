5a8aeecda5462daf9bdda0634ab0ec06d4ce9b75
Milosz08 <gilgamilosz451@gmail.com>
2021-08-08 03:20:56 +0200

    implements ProtectedLoginRoute component (without styles)

--- src/components/router/ProtectedLoginRoute.tsx
+++ src/components/router/ProtectedLoginRoute.tsx
@@ -1,1 +1,37 @@
-export {}
+import React from 'react';
+import { Redirect, Route } from 'react-router';
+
+interface PropsProvider {
+   auth: boolean;
+   setAuth: (value: boolean) => boolean;
+   handleCookie: any;
+   redirectPath: any;
+   component: any;
+}
+
+/**
+ * Komponent blokujący przejście na podstronę, jeśli użytkownik/administrator nie ma autentykacji.
+ *
+ * @param auth { boolean } - informacja o autentykacji uzytkownika/administratora.
+ * @param setAuth { (value: boolean) => boolean } - ustawianie autentykacji dla użytkownika/administratora.
+ * @param handleCookie { any } - funkcja obsługucjąca obiekt Cookie (usuwanie/dodawanie).
+ * @param redirectPath { string } - ścieżka na którą ma przenieść react router.
+ * @param Component - komponent do wyrenderowania.
+ * @param rest - reszta parametrów przekazywanych do komponentów dziedziczących.
+ */
+const ProtectedLoginRoute: React.FC<PropsProvider | any> = ({
+   auth, setAuth, handleCookie, redirectPath, component: Component, ...rest
+}) => {
+   return (
+      <Route
+         {...rest}
+         render = {props => auth ? (
+            <Component {...props} setAuth = {setAuth} handleCookie = {handleCookie} />
+         ) : (
+            <Redirect to = {redirectPath}/>
+         )}
+      />
+   );
+}
+
+export default ProtectedLoginRoute;
