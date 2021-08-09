ea6548a9458a420d6c36ca0cfe8b815748a2f9dc
Milosz08 <gilgamilosz451@gmail.com>
2021-08-09 22:36:27 +0200

    filled AdminCmsMainPanel and Navigation component (with styles)

--- src/components/pages/AdminCmsSystemPage/AdminCmsMainPanel/AdminCmsLeftNavigation.tsx
+++ src/components/pages/AdminCmsSystemPage/AdminCmsMainPanel/AdminCmsLeftNavigation.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const AdminCmsLeftNavigation = () => {
+import STATIC_NAV from '../../../../constants/navigationStatic';
+
+const { activeSection, cmsNavigation } = require('./AdminCmsMainPanel.module.scss');
+
+interface PropsProvider {
+   activeNavElm: number;
+   setActiveNavElm: Dispatch<SetStateAction<number>>
+}
+
+/**
+ * Komponent generujący nawigację po panelach administratora systemu CMS
+ *
+ * @param activeNavElm { number } - liczba mówiąca o aktywnym panelu.
+ * @param setActiveNavElm { Dispatch<SetStateAction<number>> } - funkcja ustawiająca aktywny panel.
+ */
+const AdminCmsLeftNavigation: React.FC<PropsProvider> = ({ activeNavElm, setActiveNavElm  }) => {
+
+   const generateNavStructure = STATIC_NAV.map((navElm: any, id: number) => (
+      <button
+         className = {activeNavElm === id ? activeSection : ''}
+         onClick = {() => setActiveNavElm(id)}
+         key = {id}
+      >
+         <FontAwesomeIcon
+            icon = {['fas', navElm.icon]}
+            title = {navElm.alt}
+         />
+      </button>
+   ));
+
    return (
-      <div>1234</div>
+      <nav className = {cmsNavigation}>
+         {generateNavStructure}
+      </nav>
    )
 }
 
--- src/components/pages/AdminCmsSystemPage/AdminCmsMainPanel/AdminCmsMainPanel.tsx
+++ src/components/pages/AdminCmsSystemPage/AdminCmsMainPanel/AdminCmsMainPanel.tsx
@@ -1,10 +1,110 @@
-import React from 'react';
-import { withRouter } from 'react-router';
+import React, { Fragment, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const AdminCmsMainPanel = () => {
+import COOKIES_OBJECT from '../../../../constants/allCookies';
+
+import CookiesNotification from '../../../layouts/CookiesNotification/CookiesNotification';
+import Header from '../../../layouts/Header/Header';
+import CurrentURLpath from '../../../layouts/CurrentURLpath/CurrentURLpath';
+
+import ModalsStateProvider from '../../../../contextStore/ModalsStateProvider';
+import FormDataAndValidateProvider from '../../../../contextStore/FormDataAndValidateProvider';
+import FormCalendarModalProvider from '../../../../contextStore/FormCalendarModalProvider';
+
+import UserMessageDeleteModal from './Modals/WarningDeleteModal/UserMessageDeleteModal';
+import CalendarDeleteModal from './Modals/WarningDeleteModal/CalendarDeleteModal';
+import SubjectDeleteModal from './Modals/WarningDeleteModal/SubjectDeleteModal';
+import ViewUserMessageModal from './Modals/ViewUserMessagesModal/ViewUserMessageModal';
+import AddChangeSubjectModal from './Modals/AddChangeSubjectModal/AddChangeSubjectModal';
+import AddChangeCalendarModal from './Modals/AddChangeCalendarModal/AddChangeCalendarModal';
+import AdminCmsLeftNavigation from './AdminCmsLeftNavigation';
+
+import HomePanel from './Panels/HomePanel';
+import Covid19Panel from './Panels/Covid19Panel';
+import SubjectsPanel from './Panels/SubjectsPanel';
+import ShedulePanel from './Panels/ShedulePanel';
+import CalendarPanel from './Panels/CalendarPanel';
+import UserMessagesPanel from './Panels/UserMessagePanel';
+
+const { adminLoginContainer, adminLoginWrapper } = require('./../AdminCmsLogin/AdminCmsLogin.module.scss');
+const { universalHeader, fasIcon } = require('./../../../layouts/Navigation/Navigation.module.scss');
+const { cmsSystemContainer, cmsContent, logoutButton } = require('./AdminCmsMainPanel.module.scss');
+
+interface PropsProvider {
+   setAuth: (value: boolean) => boolean;
+   handleCookie: any;
+}
+
+/**
+ * Komponent generujący panele administratora systemu CMS.
+ *
+ * @param setAuth { (value: boolean) => boolean } - funkcja ustawiająca autentykację.
+ * @param handleCookie { any } - funkcja usuwająca/dodająca obiekt Cookie.
+ */
+const AdminCmsMainPanel: React.FC<PropsProvider> = ({ setAuth, handleCookie }) => {
+
+   const [ activeNavElm, setActiveNavElm ] = useState<number>(0);
+
+   const handleLogout = () => {
+      setAuth(false);
+      handleCookie(COOKIES_OBJECT.adminSession, { path: '/', sameSite: 'strict' });
+      handleCookie(COOKIES_OBJECT.credentialsLevel, { path: '/', sameSite: 'strict' });
+   }
+
    return (
-      <div>AdminCmsMainPanel</div>
+      <Fragment>
+         <CookiesNotification/>
+         <Header ifHeaderHasRedBar = {false}/>
+         <CurrentURLpath ifImportatHeaderActive = {false}/>
+         <ModalsStateProvider>
+            <SubjectDeleteModal/>
+            <CalendarDeleteModal/>
+            <UserMessageDeleteModal/>
+            <ViewUserMessageModal/>
+            <FormDataAndValidateProvider>
+               <AddChangeSubjectModal/>
+            </FormDataAndValidateProvider>
+            <FormCalendarModalProvider>
+               <AddChangeCalendarModal/>
+            </FormCalendarModalProvider>
+            <div className = {adminLoginContainer}>
+               <div className = {adminLoginWrapper}>
+                  <section className = {universalHeader}>
+                     <h3>
+                        <FontAwesomeIcon
+                           icon = {['fas', 'industry']}
+                           className = {fasIcon}
+                        />
+                        Panel Systemu Zarządzania Treścią
+                        <aside/>
+                        <button
+                           onClick = {handleLogout}
+                           className = {logoutButton}
+                        >Wyloguj</button>
+                     </h3>
+                  </section>
+
+                  <div className = {cmsSystemContainer}>
+                     <AdminCmsLeftNavigation
+                        activeNavElm = {activeNavElm}
+                        setActiveNavElm = {setActiveNavElm}
+                     />
+                     <div className = {cmsContent}>
+                        <HomePanel activeNavElm = {activeNavElm}/>
+                        <Covid19Panel activeNavElm = {activeNavElm}/>
+                        <FormDataAndValidateProvider>
+                           <SubjectsPanel activeNavElm = {activeNavElm}/>
+                        </FormDataAndValidateProvider>
+                        <ShedulePanel activeNavElm = {activeNavElm}/>
+                        <CalendarPanel activeNavElm = {activeNavElm}/>
+                        <UserMessagesPanel activeNavElm = {activeNavElm}/>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </ModalsStateProvider>
+      </Fragment>
    );
 }
 
-export default withRouter(AdminCmsMainPanel);
+export default AdminCmsMainPanel;
