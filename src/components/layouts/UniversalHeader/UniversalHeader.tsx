6d23e339e7171ba40eccfac6adfde6863b241457
Milosz08 <gilgamilosz451@gmail.com>
2021-08-12 13:19:07 +0200

    update some Props (addHeaderDayIndicator) with styles

--- src/components/layouts/UniversalHeader/UniversalHeader.tsx
+++ src/components/layouts/UniversalHeader/UniversalHeader.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
-const { universalHeader, universalHeaderIcon, universalHeaderButton } = require('./UniversalHeader.module.scss');
+const {
+   universalHeader, universalHeaderIcon, universalHeaderButton, additionalTitleHeader
+} = require('./UniversalHeader.module.scss');
 
 interface PropsProvider {
    iconP : IconProp;
    content: string;
    ifCloseButtonVisible : boolean;
+   addHeaderDayIndicator?: string;
    setCloseButton? : () => void;
 }
 
@@ -15,12 +18,15 @@ interface PropsProvider {
  * Komponent implementujący główny header w sekcjach. W zależności od podanych propsów, header generuje się z
  * przyciskiem zamknięcia (dla okien typu modal).
  *
- * @param iconP - tablica z dwoma parametrami typu string opisującymi ikonę
- * @param content - zawartość tekstowa nagłówka
- * @param ifCloseButtonVisible - wartość boolean, wskazująca czy header powinien posiadać przycisk zamknięcia
- * @param setCloseButton - funkcja przekazywana do obsługi przycisku zamykania modala
+ * @param iconP - tablica z dwoma parametrami typu string opisującymi ikonę.
+ * @param content - zawartość tekstowa nagłówka.
+ * @param ifCloseButtonVisible - wartość boolean, wskazująca czy header powinien posiadać przycisk zamknięcia.
+ * @param setCloseButton - funkcja przekazywana do obsługi przycisku zamykania modala.
+ * @param addHeaderDayIndicator - dla modalu dodającym/modyfikującym przedmioty w planie zajęć.
  */
-const UniversalHeader: React.FC<PropsProvider> = ({ iconP, content, ifCloseButtonVisible, setCloseButton}) => {
+const UniversalHeader: React.FC<PropsProvider> = ({
+   iconP, content, ifCloseButtonVisible, setCloseButton, addHeaderDayIndicator
+}) => {
    return (
       <header className = {universalHeader}>
          <h3>
@@ -29,6 +35,7 @@ const UniversalHeader: React.FC<PropsProvider> = ({ iconP, content, ifCloseButto
                className = {universalHeaderIcon}
             />
             {content}
+            {addHeaderDayIndicator && <span className = {additionalTitleHeader}>{addHeaderDayIndicator}</span>}
             <aside/>
             {ifCloseButtonVisible && <button
                className = {universalHeaderButton}
