bf0a5abadf84dd35fd6b1905b387472c92071943
Milosz08 <gilgamilosz451@gmail.com>
2021-08-12 14:21:33 +0200

    remove unused components imports

--- src/components/router/App.tsx
+++ src/components/router/App.tsx
@@ -2,17 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import { CookiesProvider } from 'react-cookie';
 
-import CookiesNotification from '../layouts/CookiesNotification/CookiesNotification';
 import MainStoreProvider from '../../contextStore/MainStoreContext';
-import StartPage from '../pages/StartPage/StartPage';
 import CookiesObjectsProvider from '../../contextStore/CookiesObjectsProvider';
-import Slider from '../pages/StartPage/Slider/Slider';
-import Subjects from '../layouts/Subjects/Subjects';
-import CountDown from '../pages/StartPage/CountDown/CountDown';
-import CovidInfo from '../pages/StartPage/CovidInfo/CovidInfo';
-import ActualDateProvider from '../../contextStore/ActualDateProvider';
-import GotoTopButton from '../layouts/GotoTopButton/GotoTopButton';
 import LoginSessionProvider from '../../contextStore/LoginSessionProvider';
+
+import GotoTopButton from '../layouts/GotoTopButton/GotoTopButton';
 import Page from './Page';
 import DevToolsInfo from '../layouts/DevToolsInfo/DevToolsInfo';
 import Footer from '../layouts/Footer/Footer';
