7fe2acfb111292c403d9fa40362107f7ae54db12
Milosz08 <gilgamilosz451@gmail.com>
2021-08-12 14:19:12 +0200

    some code refactoring (move to separate loop function)

--- src/components/pages/AdminCmsSystemPage/AdminCmsMainPanel/Panels/HomePanel.tsx
+++ src/components/pages/AdminCmsSystemPage/AdminCmsMainPanel/Panels/HomePanel.tsx
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import classnames from "classnames";
 
 import { CookiesObjectsContext } from '../../../../../contextStore/CookiesObjectsProvider';
 import { MainStoreContext } from '../../../../../contextStore/MainStoreContext';
 
 import ChangeCredentials from './AdditionalComponents/ChangeCredentials';
-import DataLastUpdate from '../../../../layouts/DataLastUpdate/DataLastUpdate';
+import GenerateModifyElement from "./AdditionalComponents/GenerateModifyElement";
 
 const { panelContainer, panelActive } = require('./Panels.module.scss');
 const { changeCredentials, disable, disabledInfo, mainInfoContainer, separator } = require('./HomePanel.module.scss');
@@ -25,12 +26,43 @@ const HomePanel: React.FC<PropsProvider> = ({ activeNavElm }) => {
 
    const { cookie } = useContext<any>(CookiesObjectsContext);
    const { dataFetchFromServer } = useContext(MainStoreContext);
-   const { calendarRecords, sheduleSubjects, subjectsData } = dataFetchFromServer;
+   const { calendarRecords, scheduleSubjects, subjectsData } = dataFetchFromServer;
 
    const setDisabledChanges = parseInt(cookie.__credentialsLevel) !== 2 ? disable : '';
    const selectRangOfAdmin = parseInt(cookie.__credentialsLevel) === 2 ? 'główny administrator' : 'moderator';
 
    const toggleClass = activeNavElm === 0 ? panelActive : '';
+
+   const DATE_ELEMENTS_CMS = [
+      {
+         id: process.env.REACT_APP_SCHEDULE_ID,
+         dateText: 'planu zajęć',
+         countText: 'przedmiotów planu zajęć',
+         dataLength: scheduleSubjects.length
+      },
+      {
+         id: process.env.REACT_APP_CALENDAR_ID,
+         dateText: 'wpisów w kalendarzu',
+         countText: 'wpisów kalendarza',
+         dataLength: calendarRecords.length
+      },
+      {
+         id: process.env.REACT_APP_SUBJECTS_ID,
+         dateText: 'przedmiotów',
+         countText: 'przedmiotów',
+         dataLength: subjectsData.length
+      }
+   ];
+
+   const generateDataElements = DATE_ELEMENTS_CMS.map((element: any) => (
+      <GenerateModifyElement
+         key = {uuidv4()}
+         dataID = {element.id}
+         dateText = {element.dateText}
+         countText = {element.countText}
+         arrayLength = {element.dataLength}
+      />
+   ));
 
    return (
       <div className = {classnames(panelContainer, toggleClass)}>
@@ -40,35 +72,9 @@ const HomePanel: React.FC<PropsProvider> = ({ activeNavElm }) => {
                ({cookie.__credentialsLevel} stopień autoryzacji).
             </h4>
             <span className = {separator}/>
-            <p>
-               Ostatnia modyfikacja planu zajęć:{' '}
-               <DataLastUpdate
-                  dataID = {process.env.REACT_APP_SUBJECTS_ID}
-                  content = 'przedmiotów'
-                  withoutText = {true}
-               />.
-               Ilość przedmiotów planu zajęć w bazie danych: <strong>{sheduleSubjects.length}</strong>.
-            </p>
-            <p>
-               Ostatnia modyfikacja wpisów w kalendarzu:{' '}
-               <DataLastUpdate
-                  dataID = {process.env.REACT_APP_CALENDAR_ID}
-                  content = 'przedmiotów'
-                  withoutText = {true}
-               />.
-               Ilość wpisów kalendarza w bazie danych: <strong>{calendarRecords.length}</strong>.
-            </p>
-            <p>
-               Ostatnia modyfikacja przedmiotów:{' '}
-               <DataLastUpdate
-                  dataID = {process.env.REACT_APP_SUBJECTS_ID}
-                  content = 'przedmiotów'
-                  withoutText = {true}
-               />.
-               Ilość przedmiotów w bazie danych: <strong>{subjectsData.length}</strong>.
-            </p>
+            {generateDataElements}
          </div>
-         <div className = {`${changeCredentials} ${setDisabledChanges}`}>
+         <div className = {classnames(changeCredentials, setDisabledChanges)}>
             <ChangeCredentials
                ifUser = {true}
                disableButton = {parseInt(cookie.__credentialsLevel) !== 2}
