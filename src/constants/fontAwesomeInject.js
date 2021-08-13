bb2662cce3afc045f2b1c71f93509c67b8bb18bb
Milosz08 <gilgamilosz451@gmail.com>
2021-08-14 01:40:04 +0200

    add all font-awesome icons to project

--- src/constants/fontAwesomeInject.js
+++ src/constants/fontAwesomeInject.js
@@ -1,10 +1,21 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import * as Icons from '@fortawesome/free-solid-svg-icons';
-import { faReact, faNodeJs, faFacebookSquare, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import * as Regular from '@fortawesome/free-regular-svg-icons';
+import * as Brand from '@fortawesome/free-brands-svg-icons';
 
 const iconList = Object
    .keys(Icons)
    .filter(key => key !== 'fas' && key !== 'prefix')
    .map(icon => Icons[icon]);
 
-library.add(faReact, faNodeJs, faFacebookSquare, faDiscord, ...iconList);
+const iconRegular = Object
+   .keys(Regular)
+   .filter(key => key !== 'far' && key !== 'prefix')
+   .map(icon => Regular[icon]);
+
+const iconBrand = Object
+   .keys(Brand)
+   .filter(key => key !== 'fab' && key !== 'prefix')
+   .map(icon => Brand[icon]);
+
+library.add(...iconList, ...iconRegular, ...iconBrand);
