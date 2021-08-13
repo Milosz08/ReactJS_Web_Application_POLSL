a0991d5318356ea8e1f5775e9cefafe61dec018d
Milosz08 <gilgamilosz451@gmail.com>
2021-08-14 01:37:21 +0200

    add modals components and implements state provider around

--- src/components/router/App.tsx
+++ src/components/router/App.tsx
@@ -5,11 +5,16 @@ import { CookiesProvider } from 'react-cookie';
 import MainStoreProvider from '../../contextStore/MainStoreContext';
 import CookiesObjectsProvider from '../../contextStore/CookiesObjectsProvider';
 import LoginSessionProvider from '../../contextStore/LoginSessionProvider';
+import GlobalModalsStateProvider from '../../contextStore/GlobalModalsStateProvider';
 
 import GotoTopButton from '../layouts/GotoTopButton/GotoTopButton';
 import Page from './Page';
 import DevToolsInfo from '../layouts/DevToolsInfo/DevToolsInfo';
 import Footer from '../layouts/Footer/Footer';
+
+import SessionActivityCount from '../additionalComponents/SessionActivityCount';
+import CredentialSequencers from '../additionalComponents/CredentialSequencers';
+import SessionEndModal from '../layouts/SessionEndModal/SessionEndModal';
 
 import './../../constants/fontAwesomeInject';
 
@@ -22,12 +27,16 @@ const App = () => {
          <CookiesProvider>
             <CookiesObjectsProvider>
                <Router>
-                  <GotoTopButton/>
-                  <LoginSessionProvider>
-                     <Page/>
-                  </LoginSessionProvider>
-                  <DevToolsInfo/>
-                  <Footer/>
+                  <GlobalModalsStateProvider>
+                     <GotoTopButton/>
+                     <LoginSessionProvider>
+                        <SessionEndModal/>
+                        <CredentialSequencers/>
+                        <Page/>
+                     </LoginSessionProvider>
+                     <DevToolsInfo/>
+                     <Footer/>
+                  </GlobalModalsStateProvider>
                </Router>
             </CookiesObjectsProvider>
          </CookiesProvider>
