2d95500ebe8c40c923db5bdeecba0efa91451ab8
Milosz08 <gilgamilosz451@gmail.com>
2021-08-19 20:07:33 +0200

    add null return statement to the React component and some TS types

--- src/components/layouts/CredentialsSequencers/SessionActivityCount.tsx
+++ src/components/layouts/CredentialsSequencers/SessionActivityCount.tsx
@@ -41,15 +41,15 @@ interface PropsProvider {
  *
  * @param adminAuth { boolean } - props deciding whether the countdown is to be run for a user or an administrator.
  */
-const SessionActivityCount: React.FC<PropsProvider> = ({ authCredentialPerson }) => {
+const SessionActivityCount: React.FC<PropsProvider> = ({ authCredentialPerson }): null => {
 
    const { adminSessionInfo, setAdminSessionInfo } = useContext<any>(GlobalModalsStateContext);
 
    useEffect(() => {
       let index: NodeJS.Timeout;
-      let secondsSinceLastActivity = 0;
-      const maxInactivity = (60 * MAX_INACTIVITY_TIME);
-      const activity = () => secondsSinceLastActivity = 0;
+      let secondsSinceLastActivity: number = 0;
+      const maxInactivity: number = (60 * MAX_INACTIVITY_TIME);
+      const activity = (): number => secondsSinceLastActivity = 0;
 
       ACTIVITY_EVENTS.forEach((eventName: string) => document.addEventListener(eventName, activity, true));
 
