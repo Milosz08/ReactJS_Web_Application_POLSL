67dab628d187aa09a2dd931675e6cf0d8714bdad
Milosz08 <gilgamilosz451@gmail.com>
2021-08-20 20:59:29 +0200

    add additional parameters for schedule visibility

--- src/constants/updateLogsDateAsync.ts
+++ src/constants/updateLogsDateAsync.ts
@@ -6,7 +6,7 @@
  * @project_name "polsl-web-application-frontend"
  * @version "^0.1.0"
  *
- * @date final version: 08/19/2021
+ * @date final version: 08/20/2021
  */
 
 import axiosInstance from '../helpers/request';
@@ -16,6 +16,7 @@ import axiosInstance from '../helpers/request';
  */
 interface DateObjectTypes {
    updateDateFor: string;
+   scheduleBreak?: boolean;
    updateDate: {
       [value: string]: number;
    }
@@ -24,22 +25,43 @@ interface DateObjectTypes {
 /**
  * @details Async function that updates the date in the database of each section (when called, the given section ID and its name).
  *
- * @param updateFor { string } - name of the section to update
- * @param updateID { string | undefined } - section id number to update
+ * @param updateFor { string } - name of the section to update.
+ * @param updateID { string | undefined } - section id number to update.
+ * @param ifSchedule? { boolean } - (only for schedule) decides how the component is to behave in the case of a schedule.
+ * @param schedule? { boolean } - (only for schedule) selection, whether the plan should be visible whether to be hidden.
  */
-const updateDate = async (updateFor: string, updateID: string | undefined): Promise<void> => {
+const updateDate = async (
+   updateFor: string, updateID: string | undefined, ifSchedule: boolean = false, schedule: boolean = false
+): Promise<void> => {
    const date = new Date();
-   const dateObject: DateObjectTypes = {
-      updateDateFor: updateFor,
-      updateDate: {
-         day: date.getDate(),
-         month: date.getMonth() + 1,
-         year: date.getFullYear(),
-         hour: date.getHours(),
-         minutes: date.getMinutes(),
-         seconds: date.getSeconds(),
-      },
-   };
+   let dateObject: DateObjectTypes;
+   if(ifSchedule) {
+      dateObject = {
+         updateDateFor: updateFor,
+         scheduleBreak: schedule,
+         updateDate: {
+            day: date.getDate(),
+            month: date.getMonth() + 1,
+            year: date.getFullYear(),
+            hour: date.getHours(),
+            minutes: date.getMinutes(),
+            seconds: date.getSeconds(),
+         },
+      };
+   } else {
+      dateObject = {
+         updateDateFor: updateFor,
+         updateDate: {
+            day: date.getDate(),
+            month: date.getMonth() + 1,
+            year: date.getFullYear(),
+            hour: date.getHours(),
+            minutes: date.getMinutes(),
+            seconds: date.getSeconds(),
+         },
+      };
+   }
+
    await axiosInstance.put(`last-update/${updateID}`, dateObject);
 }
 
