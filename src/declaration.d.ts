a4f96e55e78610f861ae90fb70c476989b6bec7a
Milosz08 <gilgamilosz451@gmail.com>
2021-08-24 20:59:46 +0200

    add some declarations in TS dependencies

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,5 +1,9 @@
 declare module 'react-crypt-gsm';
 declare module 'react-delay-link';
+declare module 'core-js/fn/number/is-nan';
+declare module 'core-js/es7/';
+declare module 'core-js/es6/';
+declare module 'raf/polyfill';
 
 declare module '*.scss' {
    const content: Record<string, string>;
