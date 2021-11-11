a5267a6eefe9c9dbf19aa6fe1cd9c442184351d4
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-11-11 17:04:54 +0100

    prevent readable undefined object value and get actual filtered state from redux store

--- src/components/layouts/SubjectsCheckboxes/subcomponents/AllCheckboxes.tsx
+++ src/components/layouts/SubjectsCheckboxes/subcomponents/AllCheckboxes.tsx
@@ -13,21 +13,17 @@
  */
 
 import * as React from 'react';
-import { Fragment, useEffect } from 'react';
+import { Fragment, useContext, useEffect } from 'react';
 
-import useInputFilter from '../../../../helpers/hooks/useInputFilter';
-
-import { RootState } from '../../../../redux/reduxStore';
-import { useDispatch, useSelector } from 'react-redux';
-import { sortInputTypes } from '../../../../redux/apiReduxStore/types';
+import { useDispatch } from 'react-redux';
 import { searchInputs } from '../../../../redux/preferencesReduxStore/types';
-import { ApiInitialTypes } from '../../../../redux/apiReduxStore/initialState';
 import { setErrorsSearchInputs } from '../../../../redux/preferencesReduxStore/actions';
 
 import { SubjectsTilesContainer } from '../SubjectsCheckboxes.styles';
 
 import SingleSubjectElements from './SingleSubjectElements';
 import NotFindContent from '../../NotFindContent/NotFindContent';
+import { SearchingContext, SearchingTypes } from '../../../../context/searchingContext/SearchingProvider';
 
 /**
  * Component responsible for generate all subjects tiles buttons based on redux state.
@@ -35,12 +31,11 @@ import NotFindContent from '../../NotFindContent/NotFindContent';
  */
 const AllCheckboxes: React.FC = (): JSX.Element => {
 
-    const { subjectsContent }: ApiInitialTypes = useSelector((state: RootState) => state.apiReducer);
+    const { subjectsNewState } = useContext<Partial<SearchingTypes>>(SearchingContext);
 
     const dispatcher = useDispatch();
-    const subjectsNewState = useInputFilter(subjectsContent, sortInputTypes.SUBJECT_SEARCH);
 
-    const subjects = subjectsNewState.map((el, idx) => (
+    const subjects = subjectsNewState!.map((el, idx) => (
         <SingleSubjectElements
             key = {idx}
             tile = {el}
@@ -49,8 +44,8 @@ const AllCheckboxes: React.FC = (): JSX.Element => {
     ));
 
     useEffect(() => {
-        dispatcher(setErrorsSearchInputs(searchInputs.SUBJECT_SEARCH, subjectsNewState.length === 0));
-    }, [ dispatcher, subjectsNewState.length]);
+        dispatcher(setErrorsSearchInputs(searchInputs.SUBJECT_SEARCH, subjectsNewState!.length === 0));
+    }, [ dispatcher, subjectsNewState ]);
 
     return (
         <Fragment>
@@ -58,7 +53,7 @@ const AllCheckboxes: React.FC = (): JSX.Element => {
                 {subjects}
             </SubjectsTilesContainer>
             <NotFindContent
-                ifVisible = {subjectsNewState.length === 0}
+                ifVisible = {subjectsNewState!.length === 0}
                 content = 'przedmiotu'
             />
         </Fragment>
