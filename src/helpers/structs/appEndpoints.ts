68a727d6614bcd05f74644d03aa11e2e2f48d1fe
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-11-23 21:00:28 +0100

    add CMS endpoints for subpages

--- src/helpers/structs/appEndpoints.ts
+++ src/helpers/structs/appEndpoints.ts
@@ -12,6 +12,8 @@
  * governing permissions and limitations under the license.
  */
 
+import { BsBookmarkCheck, BsCalendar, BsChatDots, BsClipboard, BsLightning } from 'react-icons/all';
+
 export enum FRONT_ENDPOINTS {
     ABSOLUTE = '/',
     SCHEDULE = '/interaktywny-plan-zajęć',
@@ -33,3 +35,48 @@ export enum API_ENDPOINTS {
     CALENDAR_RECORDS = '/calendar',
     AUTHENTICATIONS = '/authentications',
 }
+
+export const CMS_ENDPOINTS = [
+    {
+        path: '/modyfikuj-zagrożenia-covid',
+        icon: 'BsShield',
+        title: 'Ograniczenia Covid 19',
+        description: 'Przejdź, aby zmodyfikować aktualne ograniczenia związane z wirusem Sars-Cov-2.',
+    },
+    {
+        path: '/modyfikuj-przedmioty',
+        icon: 'BsBookmarkCheck',
+        title: 'Przedmioty',
+        description: 'Przejdź, aby dodać, usunąć lub zmodyfikować przedmiot.',
+    },
+    {
+        path: '/modyfikuj-plan-zajęć',
+        icon: 'BsClipboard',
+        title: 'Plan Zajęć',
+        description: 'Przejdź, aby dodać, usunąć lub zmodyfikować plan zajęć.',
+    },
+    {
+        path: '/modyfikuj-kalendarz',
+        icon: 'BsCalendar',
+        title: 'Kalendarz',
+        description: 'Przejdź, aby dodać, usunąć lub zmodyfikować wpis w kalendarzu.',
+    },
+    {
+        path: '/wiadomości-użytkowników',
+        icon: 'BsEnvelope',
+        title: 'Skrzynka',
+        description: 'Przejdź, aby przejrzeć lub usunąć wiadomość od użytkownika.',
+    },
+    {
+        path: '/modyfikuj-pomoce-naukowe',
+        icon: 'BsDownload',
+        title: 'Pomoce Naukowe',
+        description: 'Przejdź, aby dodać, usunąć lub zmodyfikować linki do pomocy naukowych.',
+    },
+    {
+        path: '/modyfikuj-wpisy',
+        icon: 'BsFiles',
+        title: 'Wpisy z Aktualnościami',
+        description: 'Przejdź, aby dodać, usunąć lub zmodyfikować wpisy z aktualnościami.',
+    },
+] as const;
