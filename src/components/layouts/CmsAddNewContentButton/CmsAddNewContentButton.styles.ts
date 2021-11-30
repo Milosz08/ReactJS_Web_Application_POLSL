11f187f8b759c2be87269596373b4ff7ddfc1c50
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-11-30 09:17:51 +0100

    create button component in cms content adding new element (based props) with styles

---
+++ src/components/layouts/CmsAddNewContentButton/CmsAddNewContentButton.styles.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021, by Miłosz Gilga <https://miloszgilga.pl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+import styled from 'styled-components';
+import { button_rs } from '../../../styles/reset.styles';
+
+export const CmsAddNewContentButtonStyles = styled(button_rs)`
+    border: 2px dashed var(--navyBlueColor);
+    color: var(--navyBlueColor);
+    border-radius: 10px;
+    margin-top: 10px;
+    padding: 12px;
+    width: 100%;
+    text-align: center;
+    font-size: 1.2rem;
+    font-weight: 500;
+`;
