460dbdea5591a00343854159fde1c7942b9cc210
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-12-18 19:36:35 +0100

    create UniversalInputWithButton with styles

---
+++ src/components/layouts/UniversalInputWithButton/UniversalInputWithButton.styles.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2021, by Miłosz Gilga <https://miloszgilga.pl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+import styled from 'styled-components';
+
+import { button_rs, input_rs } from '../../../styles/reset.styles';
+import { StandardTextInput } from '../../../styles/mixins.styles';
+
+export const UniversalInputWithButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 10px 20px;
+    width: calc(100% - 40px);
+`;
+
+export const UniversalInputWithButtonLabel = styled.label`
+    width: 100%;
+    position: relative;
+`;
+
+export const UniversalInputWithButtonInput = styled(input_rs)<{ ifError: boolean }>`
+    ${props => StandardTextInput({ _ifError: props.ifError, _paddingRight: 50, _spaceUpDown: 10 })};
+    transition: ${props => props.ifError ? 'none' : 'var(--transitionDuration)'};
+    font-size: 1.1rem;
+`;
+
+export const UniversalInputWithButtonCustomButton = styled(button_rs)`
+`;
+
+export const CleanInputButtonIconWrapper = styled('div')<{ $ifError: boolean }>`
+    color: var(${props => props.$ifError ? '--redColor' : '--navyBlueColor'});
+    position: absolute;
+    margin-left: -47px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: flex;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    font-size: 1.3rem;
+    svg {
+        flex-grow: 1;
+    }
+`;
---
+++ src/components/layouts/UniversalInputWithButton/UniversalInputWithButton.tsx
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2021, by Miłosz Gilga <https://miloszgilga.pl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+import * as React from 'react';
+import { FaRegTrashAlt } from 'react-icons/all';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../redux/reduxStore';
+import { ModalsActions } from '../../../redux/modalsReduxStore/actions';
+import { ModalsInitialTypes } from '../../../redux/modalsReduxStore/initialState';
+import { allModals, allModalsInputs } from '../../../redux/modalsReduxStore/types';
+
+import {
+    CleanInputButtonIconWrapper, UniversalInputWithButtonContainer, UniversalInputWithButtonCustomButton,
+    UniversalInputWithButtonInput, UniversalInputWithButtonLabel
+} from './UniversalInputWithButton.styles';
+
+interface PropsProvider {
+    modalType: allModals;
+    inputType: allModalsInputs;
+    inputMaxLength: number;
+    placeholder?: string;
+    CustomIcon?: any;
+    CustomContent?: React.FC;
+}
+
+/**
+ * Component responsible for generating custom input with clear button. Mostly
+ * used in CMS panels and modals.
+ *
+ * @param placeholder { string? } - input placeholder ().
+ * @param modalType { allModals } - selected modal.
+ * @param inputType { allModalsInputs } - selected modal input.
+ * @param CustomIcon { any? } - custom icon component.
+ * @param CustomContent { React.FC } - custom react content.
+ * @param inputMaxLength { number } - max length of input.
+ */
+const UniversalInputWithButton: React.FC<PropsProvider> = ({
+    placeholder, modalType, inputType, CustomIcon, CustomContent, inputMaxLength
+}): JSX.Element => {
+
+    const dispatcher = useDispatch();
+
+    const initialState: ModalsInitialTypes = useSelector((state: RootState) => state.modalsReducer);
+
+    const inputField = initialState[modalType].modalInputFields![inputType];
+    const errorField = initialState[modalType].modalInputErrorsFields![inputType];
+
+    const handleInputChange = ({ target }: React.ChangeEvent<HTMLInputElement>): void => {
+        dispatcher(ModalsActions.changeModalSelectedInput(modalType, inputType, target.value));
+        dispatcher(ModalsActions.checkAllInputs(modalType));
+    };
+
+    const handleClearInput = () => {
+        dispatcher(ModalsActions.changeModalSelectedInput(modalType, inputType, ''));
+    };
+
+    return (
+        <UniversalInputWithButtonContainer>
+            <UniversalInputWithButtonLabel>
+                <UniversalInputWithButtonInput
+                    type = 'text'
+                    placeholder = {placeholder}
+                    value = {inputField}
+                    onChange = {handleInputChange}
+                    ifError = {errorField}
+                    maxLength = {inputMaxLength}
+                />
+                {CustomContent ? <CustomContent/> : <UniversalInputWithButtonCustomButton
+                    onClick = {handleClearInput}
+                    title = 'Wyczyść pole'
+                >
+                    <CleanInputButtonIconWrapper
+                        $ifError = {errorField}
+                    >
+                        {CustomIcon ? <CustomIcon/> : <FaRegTrashAlt/>}
+                    </CleanInputButtonIconWrapper>
+                </UniversalInputWithButtonCustomButton>}
+            </UniversalInputWithButtonLabel>
+        </UniversalInputWithButtonContainer>
+    );
+};
+
+export default UniversalInputWithButton;
