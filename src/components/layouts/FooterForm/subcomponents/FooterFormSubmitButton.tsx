645068e29052d6c3b617f47fdf9eb527ef8627f7
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-12-18 19:55:55 +0100

    prepare submit button function for rewrite redux state

--- src/components/layouts/FooterForm/subcomponents/FooterFormSubmitButton.tsx
+++ src/components/layouts/FooterForm/subcomponents/FooterFormSubmitButton.tsx
@@ -13,14 +13,19 @@
  */
 
 import * as React from 'react';
+import { AES } from 'crypto-js';
+
 import useFooterForm from '../../../../helpers/hooks/useFooterForm';
+import { API_ENDPOINTS } from '../../../../helpers/structs/appEndpoints';
 import useTitleFooterForm from '../../../../helpers/hooks/useTitleFooterForm';
 
-import { FormSubmitAsideText, FormSubmitButton, FormSubmitButtonContainer } from '../FooterForm.styles';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../../redux/reduxStore';
-import { sendFooterMessage } from '../../../../redux/apiReduxStore/actions';
+import { apiGetContentFromDB } from '../../../../redux/apiReduxStore/types';
+import { DbNonModalOp } from '../../../../redux/apiReduxStore/operationsForNonModals';
 import { PreferencesInitialTypes } from '../../../../redux/preferencesReduxStore/initialState';
+
+import { FormSubmitAsideText, FormSubmitButton, FormSubmitButtonContainer } from '../FooterForm.styles';
 
 /**
  * Component responsible for generating footer form structure and implementation logic from custom hooks.
@@ -28,6 +33,7 @@ import { PreferencesInitialTypes } from '../../../../redux/preferencesReduxStore
 const FooterFormSubmitButton: React.FC = (): JSX.Element => {
 
     const { footerForm }: PreferencesInitialTypes = useSelector((state: RootState) => state.preferencesReducer);
+    const hashKey: string = process.env.REACT_APP_HASH_CODE || '';
 
     const [ validateForm, clearAllInputs ] = useFooterForm();
     const [ grabber, handleShowPosMess ] = useTitleFooterForm();
@@ -37,7 +43,12 @@ const FooterFormSubmitButton: React.FC = (): JSX.Element => {
     const handleSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (validateForm()) {
-            dispatcher(sendFooterMessage(footerForm));
+            const { userNickname: userIdentity, typeOfMessage: userChoice, userMessage } = footerForm;
+            dispatcher(DbNonModalOp.addSingleNonModalElement({
+                ...footerForm, userChoice,
+                userIdentity: AES.encrypt(userIdentity, hashKey).toString(),
+                userMessage: AES.encrypt(userMessage, hashKey).toString(),
+            }, apiGetContentFromDB.USER_MESSAGES, API_ENDPOINTS.FOOTER_FORM));
             handleShowPosMess();
             clearAllInputs();
         }
