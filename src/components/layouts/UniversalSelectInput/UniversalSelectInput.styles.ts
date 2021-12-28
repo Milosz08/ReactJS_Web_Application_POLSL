ca72d4e6ed35db317fa6774d62c3b62f9cb3d717
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-12-28 22:15:54 +0100

    create UniversalSelectInput component with styles

---
+++ src/components/layouts/UniversalSelectInput/UniversalSelectInput.styles.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021, by Miłosz Gilga <https://miloszgilga.pl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+import styled from 'styled-components';
+import { MdKeyboardArrowDown } from 'react-icons/all';
+
+import { select_rs } from '../../../styles/reset.styles';
+import { StandardTextInput } from '../../../styles/mixins.styles';
+
+export const UniversalSelectInputContainer = styled.label`
+    position: relative;
+    z-index: 0;
+    margin-right: 10px;
+`;
+
+export const UniversalSelectInputElement = styled(select_rs)<{ ifError: boolean }>`
+    ${({ ifError }) => StandardTextInput({ _ifError: ifError, _paddingRight: 50, _spaceUpDown: 10 })};
+    font-size: 1.1rem;
+    text-transform: capitalize;
+    background-color: transparent;
+`;
+
+export const UniversalSelectInputArrow = styled(MdKeyboardArrowDown)`
+    position: absolute;
+    z-index: -1;
+    top: 7px;
+    right: 7px;
+    font-size: 2rem;
+    color: var(--navyBlueColor);
+`;
---
+++ src/components/layouts/UniversalSelectInput/UniversalSelectInput.tsx
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2021, by Miłosz Gilga <https://miloszgilga.pl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+import * as React from 'react';
+
+import { RootState } from '../../../redux/reduxStore';
+import { useDispatch, useSelector } from 'react-redux';
+import { ModalsActions } from '../../../redux/modalsReduxStore/actions';
+import { ModalsInitialTypes } from '../../../redux/modalsReduxStore/initialState';
+import { allModals, allModalsInputs } from '../../../redux/modalsReduxStore/types';
+
+import {
+    UniversalSelectInputArrow, UniversalSelectInputContainer, UniversalSelectInputElement
+} from './UniversalSelectInput.styles';
+
+interface PropsProvider {
+    allOptions: string[];
+    defaultOption: string;
+    modalType: allModals;
+    arrayFieldType: allModalsInputs;
+    inputFieldType: allModalsInputs;
+    itemIndex: number;
+}
+
+/**
+ * Universal component responsible for generating select input based on options array given in props.
+ *
+ * @param allOptions { string[] } - all options inserting into select input.
+ * @param defaultOption { string } - default option inserting in options input array.
+ * @param modalType { allModals } - selected modal.
+ * @param arrayFieldType { allModalsInputs } - selected input in array modal.
+ * @param inputFieldType { allModalsInputs } - selected array in modal.
+ * @param itemIndex { number } - array index element.
+ */
+const UniversalSelectInput: React.FC<PropsProvider> = ({
+    allOptions, defaultOption, modalType, arrayFieldType, inputFieldType, itemIndex
+}): JSX.Element => {
+
+    const modals: ModalsInitialTypes = useSelector((state: RootState) => state.modalsReducer);
+    const dispatcher = useDispatch();
+
+    const modalInput = modals[modalType].modalInputFields![arrayFieldType][itemIndex][inputFieldType];
+    const modalError = modals[modalType].modalInputErrorsFields![arrayFieldType][itemIndex][inputFieldType];
+
+    const addDefaultOption = [defaultOption].concat(allOptions);
+
+    const generateOptions = addDefaultOption.map(el => (
+        <option key = {el} value = {el}>
+            {el}
+        </option>
+    ));
+
+    const handleChangeInputOptions = ({ target }: React.ChangeEvent<HTMLSelectElement>): void => {
+        dispatcher(ModalsActions.changeModalSelectedInputArray(modalType, arrayFieldType, inputFieldType, itemIndex, target.value));
+    };
+
+    return (
+        <UniversalSelectInputContainer>
+            <UniversalSelectInputElement
+                ifError = {modalError}
+                value = {modalInput}
+                onChange = {handleChangeInputOptions}
+            >
+                {generateOptions}
+            </UniversalSelectInputElement>
+            <UniversalSelectInputArrow/>
+        </UniversalSelectInputContainer>
+    );
+};
+
+export default UniversalSelectInput;
